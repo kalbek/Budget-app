871fd5e8d08541e6bb6888d484b920b6769deaa2
kalbek <access2kaleb@gmail.com>
2023-08-23 10:09:17 +0300

    check for precompile.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,20 +1,16 @@
-// Import and register all your controllers from the importmap under controllers/*
+import { Application } from "@hotwired/stimulus";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+import { application as appInstance } from "controllers/application"; // Renamed to avoid conflict
 
-import { application } from "controllers/application"
-
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
-
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
+const application = Application.start();
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
 
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+eagerLoadControllersFrom("controllers", appInstance); // Load controllers using the appInstance
+
+export { application };
