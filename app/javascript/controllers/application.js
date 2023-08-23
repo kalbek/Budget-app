a59dc308f9f498d13a384a336cc40000436afc2a
kalbek <access2kaleb@gmail.com>
2023-08-23 09:09:43 +0300

    attempt solve application already has been declared error.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,24 +1,18 @@
-// app/javascript/controllers/application.js
-
-import { application } from "controllers/application";
+import { application as stimulusApplication } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 
-export const application = {
-  // Your other Stimulus controller definitions here
-
-  initialize() {
-    document.addEventListener("DOMContentLoaded", function() {
-      // Your DOMContentLoaded logic here
-      var checkboxes = document.querySelectorAll(".public-checkbox");
-      var form = document.querySelector(".recipe-form");
-      for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener("change", function() {
-          form.submit();
-        });
-      }
+document.addEventListener("DOMContentLoaded", function() {
+  // Your DOMContentLoaded logic here
+  var checkboxes = document.querySelectorAll(".public-checkbox");
+  var form = document.querySelector(".recipe-form");
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener("change", function() {
+      form.submit();
     });
   }
-};
+});
+
+stimulusApplication.initialize();
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-eagerLoadControllersFrom("controllers", application);
+eagerLoadControllersFrom("controllers", stimulusApplication);
