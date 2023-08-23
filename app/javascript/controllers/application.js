62a1e935e0480cdbb41dcf66647573aa8cc72db6
kalbek <access2kaleb@gmail.com>
2023-08-23 09:40:30 +0300

    check for precompile.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,18 +1,7 @@
-import { application as stimulusApplication } from "controllers/application";
+// app/javascript/controllers/application.js
+
+import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Your DOMContentLoaded logic here
-  var checkboxes = document.querySelectorAll(".public-checkbox");
-  var form = document.querySelector(".recipe-form");
-  for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener("change", function() {
-      form.submit();
-    });
-  }
-});
-
-stimulusApplication.initialize();
-
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-eagerLoadControllersFrom("controllers", stimulusApplication);
+eagerLoadControllersFrom("controllers", application);
