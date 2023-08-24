4f2bc7dc5f98e6076a9e2ae24ce2aac1848290f0
kalbek <access2kaleb@gmail.com>
2023-08-24 16:33:44 +0300

    Set config.assets.enabled to true.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module BlogApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.enabled = true
   end
 end
